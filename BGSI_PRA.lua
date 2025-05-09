5e00afae67211cd7e48349c9cc09c89503c9e046
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-05-09 20:18:21 +0200

    Update BGSI_PRA.lua

--- BGSI_PRA.lua
+++ BGSI_PRA.lua
@@ -1,20 +1,23 @@
+--// PIRA old script
+wait(15)
 setfpscap(5)
+
 getgenv().Config = {
     ["Hatching"] = {
         ["Rifts"] = {
-            ["Eggs"] = {"Silly Egg", "Cyber Egg"}
+            ["Eggs"] = {"Silly Egg", "Nightmare Egg"}
         },
-        ["Egg"] = "Cyber Egg"
+        ["Egg"] = "Infinity Egg"
     },
     ["Webhook"] = {
-        ["MinDifficulty"] = "10m",
+        ["MinDifficulty"] = "5m",
         ["UserID"] = "667064890359545917",
         ["URL"] = "https://discord.com/api/webhooks/1336383820353507350/xAuZhf_409qiWmrjUNp_Gfv_pSgU_vgFjBqv7Rcqy1ioqKzxgwION1xKYCKB_lhrRRj3",
     },
     ["AutoTrade"] = {
-        ["Usernames"] = {"", ""}, -- Receiver Accounts
+        ["Usernames"] = {""}, -- Receiver Accounts
         ["URL"] = ""
     },
     ["AutoEnchant"] = true -- // Enchant Team Up on all Equipped Pets
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9ba58bbb26e46302b4e601379491c5da.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6a28683e5e681161a5074613f6daf64c.lua"))()
